cb25819caaab56a47e09201bfd6d05a54ed59d51
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-13 15:19:52 +0300

    menu.py screen frames
    screen place and frame

--- menu.py
+++ menu.py
@@ -7,3 +7,11 @@ class Menu:
         # головний фрейм
         self.mainframe = ttk.Frame(self.root, relief=tk.RAISED, borderwidth=1)
         self.mainframe.grid(column=0, row=0)
+
+        # фрейм для екрану
+        self.screen_place = ttk.Frame(self.mainframe, padding="5 5 5 5", relief=tk.RAISED, borderwidth=1)
+        self.screen_place.grid(column=0, row=0)
+
+        #фрейм для вмісту екрану
+        self.screen_frame = ttk.Frame(self.screen_place, width=200, height=200, relief=tk.RAISED, borderwidth=1)
+        self.screen_frame.grid(column=1, row=0, rowspan=4, sticky="nsew")
