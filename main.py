0bb780efa55f53235ecb670d9217976a2f64917a
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-13 15:01:48 +0300

    main.py import menu

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tkinter as tk
+from menu import Menu
 class ATMApp:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("ATM App")
+        self.menu = Menu(self.root)
     def run(self):
         self.root.mainloop()
 if __name__ == "__main__":
