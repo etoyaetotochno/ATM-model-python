420fbddfa9adec2c7cfcc7ff8c17f3dc18019248
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-13 15:03:00 +0300

    menu.py mainframe

--- menu.py
+++ menu.py
@@ -3,3 +3,7 @@ from tkinter import ttk
 class Menu:
     def __init__(self, root):
         self.root = root
+
+        # головний фрейм
+        self.mainframe = ttk.Frame(self.root, relief=tk.RAISED, borderwidth=1)
+        self.mainframe.grid(column=0, row=0)
