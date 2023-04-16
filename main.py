42ffb6d3019ca7f59c79cb510cc02667dc41f144
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-16 19:31:40 +0300

    main.py fix screen grid

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class ATMApp:
         self.menu = Menu(self.root, callback=self.menu_callback)
 
         self.screen = Screen(self.menu.screen_frame)
-        self.screen.grid(sticky="nsew")
+        self.screen.grid(sticky="ew")
         self.screen.grid_columnconfigure(0, weight=1)
         self.screen.grid_rowconfigure(0, weight=1)
 
