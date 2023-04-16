fe094b2343f56d7c29967cbe940b86fd1a34e2a9
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-16 20:27:20 +0300

    screen.py fix T message grid

--- screen.py
+++ screen.py
@@ -35,7 +35,7 @@ class Screen(tk.ttk.Frame):
     def T(self, text):
         self.clear_elements()
         self.message.config(text=text)
-        self.message.grid()
+        self.message.grid(row=0, column=0, columnspan=2, rowspan=4, padx=10, pady=10)
 
     def TE(self, text):
         self.clear_elements()
