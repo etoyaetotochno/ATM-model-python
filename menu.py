eb22249119d1788d8ca0245b2354a100f33ebd96
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-16 18:58:35 +0300

    menu.py change buttons to call callback

--- menu.py
+++ menu.py
@@ -23,12 +23,12 @@ class Menu:
 
         # кнопки зліва від екрану
         for i in range(4):
-            button = tk.Button(self.screen_place, text=">>", width=10, height=2, command=lambda v="L"+str(i): self.update_screen(v))
+            button = tk.Button(self.screen_place, text=">>", width=10, height=2, command=lambda v="L"+str(i): self.button_clicked(v))
             button.grid(row=i, column=0, padx=5)
 
         # кнопки справа від екрану
         for i in range(4):
-            button = tk.Button(self.screen_place, text="<<", width=10, height=2, command=lambda v="R"+str(i): self.update_screen(v))
+            button = tk.Button(self.screen_place, text="<<", width=10, height=2, command=lambda v="R"+str(i): self.button_clicked(v))
             button.grid(row=i, column=3, padx=5)
 
         # зчитувач карток
@@ -42,19 +42,19 @@ class Menu:
         for i in range(3):
             for j in range(3):
                 value = i * 3 + j + 1
-                button = tk.Button(self.pinpad, text=str(value), width=10, height=2, command=lambda v=str(value): self.update_screen(v))
+                button = tk.Button(self.pinpad, text=str(value), width=10, height=2, command=lambda v=str(value): self.button_clicked(v))
                 button.grid(row=i, column=j, padx=5, pady=5)
 
         # бокові кнопки
         side_buttons = ["СКАС", "КОРР", "ВІДМ", "ОК"]
         for i in range(4):
-            button = tk.Button(self.pinpad, text=side_buttons[i], width=10, height=2, command=lambda v=side_buttons[i]: self.update_screen(v))
+            button = tk.Button(self.pinpad, text=side_buttons[i], width=10, height=2, command=lambda v=side_buttons[i]: self.button_clicked(v))
             button.grid(row=i, column=3, padx=5, pady=5)
 
         # нижні кнопки 0, 00
         button_text = ["", "0", "00"]
         for i in range(3):
-            button = tk.Button(self.pinpad, text=button_text[i], width=10, height=2, command=lambda v=button_text[i]: self.update_screen(v))
+            button = tk.Button(self.pinpad, text=button_text[i], width=10, height=2, command=lambda v=button_text[i]: self.button_clicked(v))
             button.grid(row=3, column=i, padx=5, pady=5)
 
         # видавач готівки
