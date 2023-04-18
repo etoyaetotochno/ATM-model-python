a0e0dd9c0d650dde34d707b84a64c3bc8abb8585
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-18 17:26:42 +0300

    create cardreader and dropdown box

--- cardreader.py
+++ cardreader.py
@@ -6,3 +6,25 @@ class Cardreader(ttk.Frame):
         ttk.Frame.__init__(self, master)
 
         self.callback = callback
+
+        self.cardreader = tk.Label(self, text="Зчитувач карток")
+        self.cardreader.grid(column=0,row=1)
+
+        self.dropdownvar = tk.StringVar()
+
+        style = ttk.Style()
+        style.configure('TCombobox', padding=(5, 5, 5, 5), font=('Helvetica', 8))
+
+        self.dropdown = ttk.Combobox(self, width=30, height=1, textvariable=self.dropdownvar)
+        self.dropdown['values'] = cards
+        self.dropdown.grid(column=0, row=2)
+
+        self.dropdown.bind("<<ComboboxSelected>>", self.on_menu_select)
+
+    def on_menu_select(self, event):
+        selected_value = self.dropdownvar.get()
+        print("Selected value:", selected_value)
+        self.callback(selected_value)
+        self.cardreader.configure(state="disabled")
+        self.dropdown.configure(state="disabled")
+
