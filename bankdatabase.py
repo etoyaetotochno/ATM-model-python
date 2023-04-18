06a3aac00edc4d32fef87c6261e5b861d1ea2a6c
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-18 17:09:00 +0300

    Create bankdatabase.py

---
+++ bankdatabase.py
@@ -0,0 +1,11 @@
+class BankDatabase:
+    def __init__(self):
+        self.parse_db()
+
+    def parse_db(self):
+        self.db = {}
+        with open('db.csv', 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                card = row['Card']
+                self.db[card] = row
