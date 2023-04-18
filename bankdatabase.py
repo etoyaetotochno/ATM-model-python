9412d22e0cd55bac1afa4f81f26660d723f1cb78
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-18 17:37:15 +0300

    bankdatabase.py check card func

--- bankdatabase.py
+++ bankdatabase.py
@@ -10,3 +10,10 @@ class BankDatabase:
             for row in reader:
                 card = row['Card']
                 self.db[card] = row
+
+    def check_card(self, card, pin):
+        if self.db[card] and self.db[card]["PIN"]==pin:
+            return True
+        else:
+            return False
+
